3859c1b783fc54cde5bc0d6db032c5a85964a3cb
YoungIm Jo <graynun@gmail.com>
2017-07-18 18:54:32 +0900

    week 01 sample completed

--- Week 01. Array methods/sample1.js
+++ Week 01. Array methods/sample1.js
@@ -29,13 +29,118 @@ const splice1 = concat1and2.splice(0, 1);
 console.log(concat1and2, splice1);
 
 
-// map: 
+const complexArray = [
+	{
+		name: '스파이더맨: 홈커밍',
+		score: 22920
+	}, {
+		name: '내 사랑',
+		score: 8416
+	}, {
+		name: '덩케르크',
+		score: 14111
+	}, {
+		name: '군함도',
+		score: 18045
+	}, {
+		name: '너의 이름은.',
+		score: 21769
+	}, {
+		name: '다크 나이트',
+		score: 6256
+	}
+];
 
-const multiplyAllResult = arr1.concat(arr2.concat(arr3.concat)).map((current, index, array) => {
 
+// map: return new array with the result of callback
+// callback안에서 return해주는 것은 새 array에 들어갈 element!
+// manipulates original array!
+
+const mapFiveStarScore = complexArray.map((current, index, array) => {
+	console.log(current);
+	console.log(index);
+	console.log(array);
+
+	current.fiveStarScore = Math.floor(current.score / 5000);
+	return current;
 });
 
+console.log(multiplyAllResult);
 
-// reduce:
 
-// filter:
+// reduce: returns the result from the reduction
+// callback에서 return 해주어야 하는 것은 accumulator!
+// manipulates original array!
+
+const addAllScore = complexArray.reduce((acc, cur, i) => {
+	console.log(accumulator);
+	console.log(current);
+	console.log(index);
+
+	return acc += cur.score;
+});
+
+const addAllScoreWithInitialValue = complexArray.reduce((acc, cur, i) => {
+	return acc += cur.score;
+}, 0);
+
+
+const reduceFiveStarScoreArray = complexArray.reduce((accumulator, current, index) => {
+	console.log(accumulator);
+	console.log(current);
+	console.log(index);
+
+	current.fiveStarScore = Math.round(current.score / 5000);
+	console.log(current.fiveStarScore);
+	delete current.score;
+
+	accumulator.push(current);
+
+	return accumulator;
+}, []);
+
+const reduceFiveStarScoreObject = complexArray.reduce((accumulator, current, index) => {
+	console.log(accumulator);
+	console.log(current);
+	console.log(index);
+	
+	current.fiveStarScore = Math.round(current.score / 5000);
+	console.log(current.fiveStarScore);
+	delete current.score;
+
+	accumulator[current.name] = current;
+
+	return accumulator;
+}, {});
+
+
+// filter: returns new array with elements passed the test
+// callback에서 return 해주어야 하는 것은 원하는 요소에 대해서 true를 반환할 조건
+// manipulates original array!
+
+const complexArray = [
+	{
+		name: '스파이더맨: 홈커밍',
+		score: 22920
+	}, {
+		name: '내 사랑',
+		score: 8416
+	}, {
+		name: '덩케르크',
+		score: 14111
+	}, {
+		name: '군함도',
+		score: 18045
+	}, {
+		name: '너의 이름은.',
+		score: 21769
+	}, {
+		name: '다크 나이트',
+		score: 6256
+	}
+];
+
+const filteredMovies = complexArray.filter((element, index, array) => {
+	element.fiveStarScore = Math.round(element.score / 5000);
+	return element.score > 20000;
+});
