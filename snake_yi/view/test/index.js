9408547e04e9aa0b28956e5ae900cd3c4760ea66
YoungIm Jo <graynun@gmail.com>
2017-09-11 22:56:17 +0900

    New: add template for mocha testing

---
+++ snake_yi/view/test/index.js
@@ -0,0 +1,10 @@
+const expect = require('expect.js');
+
+
+describe('SnakeGame', function(){
+	describe('ViewManager', function() {
+		it('should do something when something happened', function() {
+
+		});
+	});
+});
