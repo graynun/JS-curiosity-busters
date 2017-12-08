612f14860b0c6078d51edb214cc6321a785c85e1
Juyeon Han <jyhan@fastcampus.co.kr>
2017-12-08 18:06:07 +0900

    test commit

---
+++ SnakeGame_JY/snake2.js
@@ -0,0 +1,21 @@
+// 변수 초기화
+let board;
+
+let snake;
+let apple;
+
+class Square {
+  constructor(x, y) {
+    this.x    = x;
+    this.y    = y;
+    this.back = null;
+  }
+  draw() {
+    
+  }
+}
+class Snake {
+    // Initializing an object
+    constructor() {
+    }
+}
